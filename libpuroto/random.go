0b94af7b400cc5c16aa0cee96c31a665e472fe44
Damon Leven <dleven@protonmail.com>
2022-05-26 00:32:50 +0200

    fixed function not exported

--- libpuroto/random.go
+++ libpuroto/random.go
@@ -22,14 +22,14 @@ import (
 	"encoding/base64"
 )
 
-func randomBytes(length uint32) ([]byte, error) {
+func RandomBytes(length uint32) ([]byte, error) {
 	b := make([]byte, length)
 	_, err := rand.Read(b)
 	return b, err
 }
 
 func RandomString(length uint32) (string, error) {
-	raw, err := randomBytes(length)
+	raw, err := RandomBytes(length)
 	if err != nil {
 		return "", err
 	}
